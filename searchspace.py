67814d4a5a4065909c862e5c16784b9824cd7ee1
Kevin Erik Johnson <kevin@silentplanet.com>
2022-10-31 23:19:26 -0600

    add base class

---
+++ searchspace.py
@@ -0,0 +1,7 @@
+class SearchSpace:
+    '''Represents a graph or tree of nodes representing states as well as logic to determine adjace states from the current one.'''
+
+    def __init__(self):
+        pass
+
+
